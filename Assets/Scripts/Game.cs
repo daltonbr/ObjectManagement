e2fc0a3d99d29dfe0c7bd466c2a89deb5e631ada
Dalton Lima <dl@seemore.lu>
2020-08-12 23:17:50 +0200

    Spawning cubes

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] private Transform prefab;
+    [SerializeField] private KeyCode createKey = KeyCode.C;
+    
+    private void Update ()
+    {
+        if (Input.GetKeyDown(createKey))
+        {
+            CreateObject();
+        }
+    }
+    
+    private void CreateObject ()
+    {
+        Transform t = Instantiate(prefab);
+        t.localPosition = Random.insideUnitSphere * 5f;
+        t.localRotation = Random.rotation;
+        t.localScale = Vector3.one * Random.Range(0.1f, 1f);
+    }
+}
