331cb8eec8ac1001fb52967777e14959ded11da2
Dalton Lima <daltonvarussa@gmail.com>
2020-08-25 17:15:11 +0200

    Add Orbiting Camera

---
+++ Assets/OrbitingCamera.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class OrbitingCamera : MonoBehaviour
+{
+    [SerializeField]
+    private Transform focus = default;
+    
+    [Tooltip("degrees per second")]
+    [SerializeField, Range(-360f, 360f)]
+    private float angularSpeed = 20f;
+    
+    private void LateUpdate()
+    {
+        transform.RotateAround(focus.position, Vector3.up, angularSpeed * Time.deltaTime);
+    }
+    
+}
